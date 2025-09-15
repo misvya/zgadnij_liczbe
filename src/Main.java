e5d82203f772b04d87f2a9fb65a3c6fa8f5c9e43
misvya <marysia.shin@wp.pl>
2025-09-15 16:18:29 +0200

    drugi projket

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Dzień dobry");
+        //typy proste
+        //zmienne a nie obiekty
+        //male lietry
+        //typy calkowite
+        byte liczba = 124;
+        short liczba2 = 12345;
+        int liczba3 = 1213243536;
+        long liczba4 = 1213243536;
+        System.out.println("Liczba całkowita " + liczba4);
+        //typy zmiennoprzecinkowe
+        float liczba5 = 23.65f; //na końcu litera f
+        double liczba6 = 235.54654;
+        //typ logiczny
+        boolean czyPrawda = true; //false
+        //typ znakowy
+        char znak = 'a';
     }
 }
